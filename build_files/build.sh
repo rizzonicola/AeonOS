8b35704a634939cf118a6b59c5f5225ba14842e8
rizzonicola <rizzo.nicola@proton.me>
2026-07-15 17:59:04 +0200

    Update package installations and autostart settings
    Removed firefox and installed niri, dms, brave-origin, and kde-connect. Added autostart configuration for kde.xwaylandvideobridge.

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,8 +13,11 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux
-
+dnf5 remove -y firefox
+dnf5 install -y niri
+dnf5 install -y dms
+dnf5 install -y brave-origin.x86_64
+dnf5 install -y kde-connect
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -23,5 +26,13 @@ dnf5 install -y tmux
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
+
+mkdir -p /etc/xdg/autostart
+
+cp /usr/share/applications/org.kde.xwaylandvideobridge.desktop /etc/xdg/autostart/ 2>/dev/null || true
+
+if [ -f /etc/xdg/autostart/org.kde.xwaylandvideobridge.desktop ]; then
+    echo "OnlyShowIn=KDE;" >> /etc/xdg/autostart/org.kde.xwaylandvideobridge.desktop
+fi
 
 systemctl enable podman.socket
